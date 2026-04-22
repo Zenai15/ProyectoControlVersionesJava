4976dabe32df27d2ed6801710a599873375fc862
Guille <guillegrear@gmail.com>
2026-04-22 16:41:16 +0200

    Cambiar saludo a 2º de dam

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 public class Main {
-public static void main(String[] args) {
-System.out.println("Hola, control de Git");
+    public static void main(String[] args) {
+        System.out.println("Hola, control de Git");
+    }
 }
